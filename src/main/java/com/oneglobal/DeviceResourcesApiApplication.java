458e0403dfa7171e165d26ea3289bf17617eada1
Rosemberg Bezerra <rosemberg.bezerra-ext@jci.com>
2026-02-16 17:38:37 +0000

    chore: creating application spring and Gradle configurations

---
+++ src/main/java/com/oneglobal/DeviceResourcesApiApplication.java
@@ -0,0 +1,13 @@
+package com.oneglobal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeviceResourcesApiApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DeviceResourcesApiApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/oneglobal/DeviceResourcesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oneglobal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeviceResourcesApiApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
